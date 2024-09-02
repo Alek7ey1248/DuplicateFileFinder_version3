295d5131b6c1e621158cba26270a9cf445d4d841
Alek7ey1248 <alek7ey1248@gmail.com>
2024-09-02 22:48:48 +0300

    ffff

--- src/TesterTime.java
+++ src/TesterTime.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -11,22 +13,24 @@ public class TesterTime {
 
     // Время выполнения метода walkFileTree класса FileDuplicateFinder
     // @param parths - пути к папкам
-    public void timeWalkFileTree(String[] parths) throws IOException {
+    public void timeWalkFileTree(String[] paths) throws IOException {
 
         // Засекаем время выполнения метода walkFileTree
         long startTime = System.currentTimeMillis();
 
         FileDuplicateFinder finder = new FileDuplicateFinder();
         Map<Long, List<Path>> filesBySize = new HashMap<>();
-        String parth = parths[0]; //  берем только первый путь так как в методе walkFileTree только один путь
-        finder.walkFileTree(parth, filesBySize);
+        String directory = paths[0]; //  берем только первый путь так как в методе walkFileTree только один путь
+        finder.walkFileTree(directory, filesBySize);
 
         long endTime = System.currentTimeMillis();
         long duration = (long) ((endTime - startTime)/ 1000.0);
+
+        //long sizePath = getDirectorySize(Paths.get(directory));
+
         System.out.println();
-        System.out.println("-------------------------------------------------");
-        System.out.println("Путь: " + Arrays.toString(parths) + " ---");
-        System.out.println("Время выполнения walkFileTree " + duration + " милисекунд       " + (long)duration/1000.0 + " секунд");
+        System.out.println("Путь: " + directory + " ---");
+        System.out.println("Время выполнения walkFileTree " + duration + " секунд       " + (long)duration * 1000.0 + " милисекунд");
 
         // Подсчет количества файлов
         int countFiles = 0;
@@ -39,17 +43,63 @@ public class TesterTime {
         System.out.println();
     }
 
+
+    // Тайминги метода areFilesEqual - сравнение файлов
+    public void timeAreFilesEqual(Path file1, Path file2) throws IOException {
+        // Засекаем время выполнения метода areFilesEqual
+        long startTime = System.currentTimeMillis();
+
+        System.out.println("-------------------------------------------------");
+
+        FileComparator fileComparator = new FileComparator();
+
+        fileComparator.areFilesEqual(file1, file2);
+
+        long endTime = System.currentTimeMillis();
+        long duration = (long) ((endTime - startTime)/ 1000.0);
+        System.out.println("Время выполнения areFilesEqual на сравнении файлов " + file1.getFileName() + " и " + file2.getFileName() + " , размер файлов - " + Files.size(Path.of(file1.toString())) + " --- " + duration + " секунд       " + (long)duration * 1000.0 + " милисекунд");
+    }
+
+
+
     public static void main(String[] args) throws IOException {
 
         // тайминги метода walkFileTree для разных папок
         System.out.println();
         System.out.println("Тайминги метода walkFileTree для разных папок");
         TesterTime tester = new TesterTime();
+
+        System.out.println();
+        System.out.println("------------------------------------------");
+        System.out.println(" Размер - 37 объектов, всего 6,7 ГБ ");
         tester.timeWalkFileTree(new String[] {"/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder"});
+
+        System.out.println();
+        System.out.println("------------------------------------------");
+        System.out.println(" Размер - 1183 объекта, всего 10,5 ГБ ");
         tester.timeWalkFileTree(new String[] {"/home/alek7ey/Рабочий стол"});
+
+        System.out.println();
+        System.out.println("------------------------------------------");
+        System.out.println(" Размер - 5933 объекта, всего 289,7 МБ ");
         tester.timeWalkFileTree(new String[] {"/home/alek7ey/IdeaProjects"});
+
+        System.out.println();
+        System.out.println("------------------------------------------");
+        System.out.println(" Размер - 169805 объектов, всего 102,2 ГБ ");
         tester.timeWalkFileTree(new String[] {"/home/alek7ey"});
-        tester.timeWalkFileTree(new String[] {"/home"});
+
+
+
+        // тайминги метода areFilesEqual - сравнение файлов
+        System.out.println();
+        System.out.println("******************************************");
+        System.out.println("*******************************************");
+        System.out.println("Тайминги метода areFilesEqual - сравнение файлов");
+        System.out.println();
+        tester.timeAreFilesEqual(Path.of("/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/.bashrc"), Path.of("/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/.bashrc (копия)"));
+        tester.timeAreFilesEqual(Path.of("/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test11/photo_2021-12-09_16-12-54.jpg"), Path.of("/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test11/test12/photo_2021-12-09_16-12-54 (копия).jpg"));
+        tester.timeAreFilesEqual(Path.of("/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test11/test12/test13/фильм про солдат (копия)"), Path.of("/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test21/фильм про солдат"));
        }
 
 }
